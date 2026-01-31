ce744fb401cd98b4e35c276c07da52fd42d79053
noobaritra101-prog <noobaritra.101@gmail.com>
2026-01-31 21:40:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@ import os
 
 # --- YOU MUST FILL THESE IN ---
 # Get API_ID and API_HASH from https://my.telegram.org
-API_ID = 1234567               # Replace with your API ID (Integer)
-API_HASH = "YOUR_API_HASH"     # Replace with your API Hash (String)
+API_ID = 1747534               # Replace with your API ID (Integer)
+API_HASH = "5a2684512006853f2e48aca9652d83ea"     # Replace with your API Hash (String)
 
 # Get this from @BotFather
-BOT_TOKEN = "YOUR_BOT_TOKEN"   # Replace with your Bot Token
+BOT_TOKEN = "8504517756:AAERpkSmmhxolu5lgc_VNgL-vJiGHUj3coE"   # Replace with your Bot Token
 
 # Your Numeric Telegram ID (Use @userinfobot to find it)
-OWNER_ID = 123456789           # Replace with your Admin ID
+OWNER_ID = 572621020           # Replace with your Admin ID
 
 # --- TARGETS ---
 TARGET_BOT = "@roronoa_zoro_robot"
